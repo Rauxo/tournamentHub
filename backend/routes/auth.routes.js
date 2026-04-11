fe77d840ceb77c7eb1f6889fd699f141d287161e
Rauxo <rahulsaikia682@gmail.com>
2026-04-12 00:39:03 +0530

    Implement user account creation and organization model with validation

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router(); // Create a new router object
+
+
+router.post('/create');
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,14 @@ require('dotenv').config();
 const app = express();
 const PORT =  process.env.PORT;
 
-app.get('/',(req,res)=>{
+
+app.use(express.json()); // Parses incoming JSON requests
+// For parsing application/x-www-form-urlencoded (standard HTML forms)
+app.use(express.urlencoded({ extended: true })); 
+
+app.use('/api/v1/auth');
+
+app.get('/api/v1',(req,res)=>{
     res.send(`API is Running  ON port ${PORT}`);
 })
 app.listen(PORT, () => {
