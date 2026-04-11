55480b5025aa74d8e008e19aec9a7a61ade82458
Rauxo <rahulsaikia682@gmail.com>
2026-04-11 23:46:23 +0530

    Welcome to the Tournament hub

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT =  process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send(`API is Running  ON port ${PORT}`);
+})
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
