b0b3213e98ae238195af31c210d535d28812e9a0
Rauxo <rahulsaikia682@gmail.com>
2026-04-12 14:50:35 +0530

    Refactor account creation and login functionality with improved error handling and validation

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,16 +1,77 @@
-exports.createAccount = async(req,res)=>{
-    const {orgName,orgMail,location,password} = req.body;
+const organizationModel = require("../models/organization.model");
+const bcrypt = require("bcrypt");
 
-    if(!orgName || !orgMail || !location || !password){
-        return res.status(400).json({
-            message:"All fields are required ."
-        })
+//create Account
+exports.createAccount = async (req, res) => {
+  //getting values from user
+  const { orgName, orgMail, location, password } = req.body;
+
+  //check All Fields or not
+  if (!orgName || !orgMail || !location || !password) {
+    return res.status(400).json({
+      message: "All fields are required .",
+    });
+  }
+  try {
+    //check is mail exist or not
+    const existingMail = await organizationModel.findOne({ orgMail });
+    if (existingMail) {
+      return res.status(400).json({ message: "This Mail already exists" });
     }
-    try {
-        
-    } catch (error) {
-        return res.status(500).json({
-            message:"Something went wrong. Please try again later."
-        })
+    //hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //create new Organization
+    const newOrganization = new organizationModel({
+      orgName,
+      orgMail,
+      location,
+      password: hashedPassword,
+    });
+
+    //Save Organization
+    await newOrganization.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong. Please try again later.",
+    });
+  }
+};
+
+//Login User
+exports.login = async (req, res) => {
+  try {
+    //getting values from user
+    const { orgMail, password } = req.body;
+
+    //check All Fields or not
+    if (!orgMail || !password) {
+      return res.status(400).json({
+        message: "All fields are required .",
+      });
     }
-}
+
+    const Organization = await organizationModel.findOne({ orgMail });
+    if (!Organization) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, Organization.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const payload = { id: user._id, email: user.email };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong. Please try again later.",
+    });
+  }
+};
