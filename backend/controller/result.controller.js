d97165ce43c08a392e5852e79a575e21fb60675b
Rauxo <rahulsaikia682@gmail.com>
2026-04-13 17:29:48 +0530

    Implement delete and update functionality for results; enhance error handling and response messages

--- backend/controller/result.controller.js
+++ backend/controller/result.controller.js
@@ -35,20 +35,57 @@ exports.addResult = async (req, res) => {
   }
 };
 
-exports.deleteResult = async (req, res) => {};
-
-exports.updateResult = async (req, res) => {};
-exports.getResult = async (req, res) => {
-  const { id } = req.params;
-  const tournament = await tournamentModel.findById(id);
-  if (!tournament) {
-    return res.status(404).json({
-      message: "Tournament Not Found.",
+exports.deleteResult = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).json({
+        message: "Result Not Found",
+      });
+    }
+    const result = await resultModel.findByIdAndDelete(id);
+    return res.status(200).json({
+      message: "Result deleted Successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
     });
   }
-  const result = await resultModel.find({ tournament: id });
-  res.status(201).json({
-    message: "Successfully Fetch the Result",
-    data: result,
-  });
+};
+
+exports.updateResult = async (req, res) => {
+  try {
+    const updatedResult = await resultModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+    if (!updatedResult) return res.status(404).json({ message: "Not found" });
+    res.status(200).json(updatedResult);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+exports.getResult = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const tournament = await tournamentModel.findById(id);
+    if (!tournament) {
+      return res.status(404).json({
+        message: "Tournament Not Found.",
+      });
+    }
+    const result = await resultModel.find({ tournament: id });
+    res.status(201).json({
+      message: "Successfully Fetch the Result",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
